febadeacc815137a86ccbd98f979e137e7da2202
rajkumar <rajkumar.gootam@gmail.com>
2016-03-16 18:26:47 +0530

    Add ability to create students

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,24 @@
+class StudentsController < ApplicationController
+  before_action :fetch_section
+  def index
+    @students = @section.students
+  end
+
+  def new
+    @student = @section.students.new
+  end
+  def create
+    @student = @section.students.new(students_params)
+    @save_success = @student.save
+  end
+  end
+
+private
+def fetch_section
+  @klass = Klass.find(params[:klass_id])
+  @section = @klass.sections.find(params[:section_id])
+end
+def students_params
+  params.require(:student).permit(:name, :fathers_name, :gender, :email, :dob, :phone, :address)
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'students/index'
+
   get 'sections/index'
 
   resources :klasses do
-    resources :sections
+    resources :sections do
+      resources :students
+    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
